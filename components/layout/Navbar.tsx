1ea9ccaf906bc55bb04c53a8348e6876357c14e3
Crl0sDEV <sandrinocarlosmiguel@gmail.com>
2026-03-05 17:02:48 +0800

    update

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -35,7 +35,6 @@ export default function Navbar() {
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between">
           
-          {/* LOGO */}
           <a href="#" className="relative flex items-center">
             <Image 
                 src="/rolyo-logo.png" 
